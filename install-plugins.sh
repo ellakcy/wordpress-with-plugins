f31ea9e5eb770e441ee1360f430f748801ab9b76
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-02-19 14:44:55 +0200

    Installing plugin for cookie notice

--- install-plugins.sh
+++ install-plugins.sh
@@ -9,23 +9,26 @@ if [ $(${COMMAND} plugin is-installed hello) ]; then
   ${COMMAND} plugin delete hello
 fi
 
-if [ $(${COMMAND} plugin is-installed hello) ]; then
-  echo "Removing useless plugin akismet"
-  ${COMMAND} plugin delete akismet
-fi
-
 if [ $(${COMMAND} plugin is-installed wp-piwik) ]; then
-  echo "Install piwik plugin for web analytics"
+  echo "Update piwik plugin for web analytics"
   ${COMMAND} plugin update wp-piwik --activate
 else
-  echo "Update piwik plugin for web analytics"
+  echo "Install piwik plugin for web analytics"
   ${COMMAND} plugin install wp-piwik --activate
 fi
 
 if [ $(${COMMAND} plugin is-installed twitter) ]; then
-  echo "Install twitter plugin for providing a social appearance to the site"
+  echo "Update twitter plugin for providing a social appearance to the site"
   ${COMMAND} plugin update twitter --activate
 else
-  echo "Update twitter plugin for providing a social appearance to the site"
+  echo "Install twitter plugin for providing a social appearance to the site"
   ${COMMAND} plugin install twitter --activate
+fi
+
+if [ $(${COMMAND} plugin is-installed cookie-notice) ]; then
+  echo "Update cookie notice plugin in order to sho information regarding cookies"
+  ${COMMAND} plugin update cookie-notice --activate
+else
+  echo "Install cookie notice plugin in order to sho information regarding cookies"
+  ${COMMAND} plugin install cookie-notice --activate
 fi
