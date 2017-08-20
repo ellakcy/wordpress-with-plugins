83b6c0cb5c28443196e5e42c507e3c8b57f41675
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-08-20 23:25:23 +0300

    Wordpress works

--- wp_post_entrypoint.sh
+++ wp_post_entrypoint.sh
@@ -2,18 +2,12 @@
 
 WORDPRESS_PATH="/var/www/html"
 
+WORDPRESS_URL=$(php -r "echo preg_replace('#^http?://|\"|\'#', '', rtrim('${WORDPRESS_URL}','/'));")
 #Generate default user
 if [ ! $(wp --path=${WORDPRESS_PATH} --allow-root core is-installed)]; then
  echo "Generating a default user."
  wp --path=${WORDPRESS_PATH} --allow-root --admin_user="${WORDPRESS_ADMIN_USERNAME}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --title="${WORDPRESS_TITLE}" --url="${WORDPRESS_URL}" core install
 fi
-
-version=$(wp --path=${WORDPRESS_PATH} --allow-root core version)
-wp --path=${WORDPRESS_PATH} --allow-root core check-update --format=count --fields=version
-# if [ $version != ${WORDPRESS_VERSION} ]; then
-# 	echo "Outdated version try to update"
-# 	wp --path=${WORDPRESS_PATH} --allow-root core update
-# fi
 
 echo "Updating existing plugins and themes"
 wp --path=${WORDPRESS_PATH} --allow-root plugin update-all
@@ -23,6 +17,8 @@ echo "Fixing Permissions"
 chown www-data:www-data -R .
 find ${WORDPRESS_PATH} -iname "*.php" | xargs chmod +x
 
+site_url=$(wp option get siteurl)
+echo "Your site will be served via: "${site_url}
 
 #Installing 3rd party plugins
 /usr/local/bin/install-plugins.sh
