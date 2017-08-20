70814ab421cacab4d67b82c6cf0ee7aacc44497c
Dimitrios Desyllas (pc_magas) <you@example.com>
2017-08-20 19:32:45 +0300

    Set nginx configuration

--- wp_post_entrypoint.sh
+++ wp_post_entrypoint.sh
@@ -2,14 +2,14 @@
 
 WORDPRESS_PATH="/var/www/html"
 
-echo "Fixinf permissions"
-find ${WORDPRESS_PATH} -iname "*.php" | xargs chmod +x
-
 #Generate default user
 if [ ! $(wp --path=${WORDPRESS_PATH} --allow-root core is-installed)]; then
  echo "Generating a default user."
  wp --path=${WORDPRESS_PATH} --allow-root --admin_user="${WORDPRESS_ADMIN_USERNAME}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --title="${WORDPRESS_TITLE}" --url="${WORDPRESS_URL}" core install
 fi
+
+echo "define('WP_HOME', '${WORDPRESS_PATH}');" >> ${WORDPRESS_PATH}/wp-config.php
+echo "define('WP_SITEURL','$WORDPRESS_PATH');" >> ${WORDPRESS_PATH}/wp-config.php
 
 version=$(wp --path=${WORDPRESS_PATH} --allow-root core version)
 
@@ -24,6 +24,8 @@ wp --path=${WORDPRESS_PATH} --allow-root theme update-all
 
 echo "Fixing Permissions"
 chown www-data:www-data -R .
+find ${WORDPRESS_PATH} -iname "*.php" | xargs chmod +x
+
 
 #Installing 3rd party plugins
 /usr/local/bin/install-plugins.sh
